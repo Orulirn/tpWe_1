5e94f1676ea668d5bb0ab930f170025e463e6e3a
Orulirn <ewencarre@free.fr>
2025-09-18 17:05:00 +0200

    DnD class creation

--- interaction.js
+++ interaction.js
@@ -1,15 +1,52 @@
 
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
-function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+class DnD{
+    constructor(canvas){
+        this.x_init = 0;
+        this.y_init = 0;
+        this.x_final = 0;
+        this.y_final = 0;
+        this.canvas = canvas;
+        this.isPressed = false;
 
-	// Developper les 3 fonctions gérant les événements
+        this.onMouseDown = this.onMouseDown.bind(this);
+        this.onMouseMove = this.onMouseMove.bind(this);
+        this.onMouseUp = this.onMouseUp.bind(this);
 
-	// Associer les fonctions précédentes aux évènements du canvas.
-};
+        this.canvas.addEventListener('mousedown', this.onMouseDown);
+        this.canvas.addEventListener('mousemove', this.onMouseMove);
+        this.canvas.addEventListener('mouseup', this.onMouseUp);
+    }
 
+    onMouseUp(evt){
+        if (this.isPressed) {
+            var mouseCoord = getMousePosition(this.canvas,evt);
+            this.x_final = mouseCoord.x;
+            this.y_final = mouseCoord.y;
+            this.isPressed = false;
+            console.log("mouseUp", mouseCoord);
+        }
 
+    }
+
+    onMouseDown(evt){
+        var mouseCoord = getMousePosition(this.canvas,evt);
+        this.isPressed = true;
+        this.x_init = mouseCoord.x;
+        this.y_init = mouseCoord.y;
+        console.log("mouseDown", mouseCoord);
+    }
+
+    onMouseMove(evt){
+        if (this.isPressed) {
+            var mouseCoord = getMousePosition(this.canvas,evt);
+            this.x_final = mouseCoord.x;
+            this.y_final = mouseCoord.y;
+            console.log("mouseMove", mouseCoord);
+        }
+    }
+}
 // Place le point de l'événement evt relativement à la position du canvas.
 function getMousePosition(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
